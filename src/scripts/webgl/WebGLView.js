35d6cfefcbdace6424958a46b2c9fff24e299b48
Paul <pfreema1@gmail.com>
2019-11-08 21:32:28 -0500

    wtf

--- src/scripts/webgl/WebGLView.js
+++ src/scripts/webgl/WebGLView.js
@@ -1,178 +1,282 @@
 import * as THREE from 'three';
 import glslify from 'glslify';
-
 import {
-  EffectComposer,
-  BrightnessContrastEffect,
-  EffectPass,
-  RenderPass,
-  ShaderPass,
-  BlendFunction
+	EffectComposer,
+	BrightnessContrastEffect,
+	EffectPass,
+	RenderPass,
+	ShaderPass,
+	BlendFunction
 } from 'postprocessing';
-
 import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls.js';
-import { MeshDepthMaterial } from 'three';
+import { FaceNormalsHelper } from 'three';
+
 
 function remap(t, old_min, old_max, new_min, new_max) {
-  let old_range = old_max - old_min;
-  let normalizedT = t - old_min;
-  let normalizedVal = normalizedT / old_range;
-  let new_range = new_max - new_min;
-  let newVal = normalizedVal * new_range + new_min;
-  return newVal;
+	let old_range = old_max - old_min;
+	let normalizedT = t - old_min;
+	let normalizedVal = normalizedT / old_range;
+	let new_range = new_max - new_min;
+	let newVal = normalizedVal * new_range + new_min;
+	return newVal;
 }
 
 export default class WebGLView {
-  constructor(app) {
-    this.app = app;
+	constructor(app) {
+		this.app = app;
 
-    this.initThree();
-    this.initObjects();
-    this.initLights();
-    this.initControls();
-    this.initPostProcessing();
-  }
+		this.initThree();
+		this.initParticlesRenderTarget();
+		this.initObjects();
+		this.initLights();
+		this.initControls();
+		this.initPostProcessing();
+		this.initRenderTri();
+	}
 
-  initThree() {
-    this.scene = new THREE.Scene();
 
-    this.camera = new THREE.PerspectiveCamera(
-      50,
-      window.innerWidth / window.innerHeight,
-      0.01,
-      100
-    );
-    this.camera.position.z = 30;
+	initThree() {
+		this.scene = new THREE.Scene();
 
-    this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+		// this.camera = new THREE.PerspectiveCamera(
+		// 	50,
+		// 	window.innerWidth / window.innerHeight,
+		// 	0.01,
+		// 	100
+		// );
+		this.camera = new THREE.OrthographicCamera();
+		// this.camera.position.z = 0;
 
-    this.clock = new THREE.Clock();
-  }
+		this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 
-  initControls() {
-    this.trackball = new TrackballControls(
-      this.camera,
-      this.renderer.domElement
-    );
-    this.trackball.rotateSpeed = 2.0;
-    this.trackball.enabled = true;
-  }
+		this.clock = new THREE.Clock();
+	}
 
-  initLights() {
-    this.pointLight = new THREE.PointLight(0xff0000, 1, 100);
-    this.pointLight.position.set(0, 0, 50);
-    this.scene.add(this.pointLight);
-  }
+	initRenderTri() {
+		// mostly taken from here: https://medium.com/@luruke/simple-postprocessing-in-three-js-91936ecadfb7
 
-  initObjects() {
-    this.particleCount = 100;
-    this.particles = [];
+		this.resize();
+		const geometry = new THREE.BufferGeometry();
 
-    for (let i = 0; i < this.particleCount; i++) {
-      let mesh = this.createMesh();
-      this.randomizeTransform(mesh);
-      this.scene.add(mesh);
+		// triangle in clip space coords
+		const vertices = new Float32Array([
+			-1.0, -1.0, 3.0, -1.0, -1.0, 3.0
+		]);
 
-      this.particles.push(mesh);
-    }
-  }
+		geometry.addAttribute('position', new THREE.BufferAttribute(vertices, 2));
 
-  randomizeTransform(mesh) {
-    /*
-		x range:  -30 to 30
-		y range:  -15 to 15
-		z range: 10 to -50
-	*/
-    mesh.position.x = remap(Math.random(), 0, 1, -30, 30);
-    mesh.position.y = remap(Math.random(), 0, 1, -15, 15);
-    mesh.position.z = remap(Math.random(), 0, 1, -20, 10);
+		const resolution = new THREE.Vector2();
+		this.renderer.getDrawingBufferSize(resolution);
 
-    mesh.rotation.x = Math.random() * 2 * Math.PI;
-    mesh.rotation.y = Math.random() * 2 * Math.PI;
-    mesh.rotation.z = Math.random() * 2 * Math.PI;
-  }
+		this.RenderTriTarget = new THREE.WebGLRenderTarget(resolution.x, resolution.y, {
+			format: THREE.RGBFormat,
+			stencilBuffer: false,
+			depthBuffer: true
+		});
 
-  updateParticles() {
-    console.log('foo');
-    for (let i = 0; i < this.particleCount; i++) {
-      let particle = this.particles[i];
+		let material = new THREE.RawShaderMaterial({
+			fragmentShader: `
+				precision highp float;
+				uniform sampler2D uScene;
+				uniform vec2 uResolution;
+				
+				void main() {
+					vec2 uv = gl_FragCoord.xy / uResolution.xy;
+					vec3 color = vec3(uv, 1.0);
+					color = texture2D(uScene, uv).rgb;
+					
+					// Do your cool postprocessing here
+					// color.r += sin(uv.x * 50.0);
+					color.r += step(0.5, uv.x);
+					
+					gl_FragColor = vec4(color, 1.0);
+				}
+			`,
+			vertexShader: `
+				precision highp float;
+				attribute vec2 position;
+				
+				void main() {
+					// Look ma! no projection matrix multiplication,
+					// because we pass the values directly in clip space coordinates.
+					gl_Position = vec4(position, 1.0, 1.0);
+				}
+			`,
+			uniforms: {
+				uScene: {
+					type: 't',
+					value: this.particlesRt.texture
+				},
+				uResolution: { value: resolution }
+			}
+		});
 
-      particle.position.y += Math.random() * 0.03 + 0.01;
-      particle.rotation.x += Math.random() * 0.01;
-      particle.rotation.z += Math.random() * 0.01;
-    }
-  }
+		console.log(this.particlesRt.texture);
 
-  createMesh() {
-    let geo = new THREE.TetrahedronBufferGeometry(1, 0);
-    // let mat = new THREE.MeshPhongMaterial();
-    // mat.shininess = 100;
-    let mat = new THREE.MeshPhysicalMaterial({
-      roughness: 0.5,
-      metalness: 0.3,
-      reflectivity: 1,
-      clearcoat: 1
-    });
-    return new THREE.Mesh(geo, mat);
-  }
+		let renderTri = new THREE.Mesh(geometry, material);
+		renderTri.frustumCulled = false;
+		this.scene.add(renderTri);
+	}
 
-  initPostProcessing() {
-    this.composer = new EffectComposer(this.renderer);
-    this.composer.enabled = false;
+	initParticlesRenderTarget() {
+		this.particlesRt = new THREE.WebGLRenderTarget(window.innerWidth, window.innerHeight);
+		this.particlesRtCamera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.01, 100);
+		this.particlesRtCamera.position.z = 30;
 
-    const renderPass = new RenderPass(this.scene, this.camera);
-    renderPass.renderToScreen = false;
+		this.particlesRtScene = new THREE.Scene();
 
-    const contrastEffect = new BrightnessContrastEffect({ contrast: 1 });
-    const contrastPass = new EffectPass(this.camera, contrastEffect);
-    contrastPass.renderToScreen = true;
+	}
 
-    this.composer.addPass(renderPass);
-    this.composer.addPass(contrastPass);
+	initControls() {
+		this.trackball = new TrackballControls(
+			this.camera,
+			this.renderer.domElement
+		);
+		this.trackball.rotateSpeed = 2.0;
+		this.trackball.enabled = true;
+	}
 
-    // kickstart composer
-    this.composer.render(1);
-  }
+	initLights() {
+		this.pointLight = new THREE.PointLight(0xff0000, 1, 100);
+		this.pointLight.position.set(0, 0, 50);
+		this.scene.add(this.pointLight);
+	}
 
-  // ---------------------------------------------------------------------------------------------
-  // PUBLIC
-  // ---------------------------------------------------------------------------------------------
+	initObjects() {
+		this.particleCount = 100;
+		this.particles = [];
 
-  update() {
-    const delta = this.clock.getDelta();
-    const time = performance.now() * 0.0005;
+		for (let i = 0; i < this.particleCount; i++) {
+			let mesh = this.createMesh();
+			this.randomizeTransform(mesh);
+			this.addAttributes(mesh);
+			this.particlesRtScene.add(mesh);
+			// this.scene.add(mesh);
 
-    if (this.particleCount) {
-      this.updateParticles();
-    }
+			this.particles.push(mesh);
+		}
+	}
 
-    if (this.trackball) this.trackball.update();
-  }
+	addAttributes(mesh) {
+		mesh.speed = {
+			rotation: Math.random() * 0.01,
+			y: Math.random() * 0.03 + 0.01
+		};
+	}
 
-  draw() {
-    if (this.composer && this.composer.enabled) this.composer.render();
-    else this.renderer.render(this.scene, this.camera);
-  }
+	randomizeTransform(mesh) {
+		/*
+			x range:  -30 to 30
+			y range:  -15 to 15
+			z range: 10 to -50
+		*/
+		mesh.position.x = remap(Math.random(), 0, 1, -30, 30);
+		mesh.position.y = remap(Math.random(), 0, 1, -15, 15);
+		mesh.position.z = remap(Math.random(), 0, 1, -20, 10);
 
-  // ---------------------------------------------------------------------------------------------
-  // EVENT HANDLERS
-  // ---------------------------------------------------------------------------------------------
+		mesh.rotation.x = Math.random() * 2 * Math.PI;
+		mesh.rotation.y = Math.random() * 2 * Math.PI;
+		mesh.rotation.z = Math.random() * 2 * Math.PI;
+	}
 
-  resize() {
-    if (!this.renderer) return;
-    this.camera.aspect = window.innerWidth / window.innerHeight;
-    this.camera.updateProjectionMatrix();
+	updateParticles() {
+		for (let i = 0; i < this.particleCount; i++) {
+			let particle = this.particles[i];
 
-    this.fovHeight =
-      2 *
-      Math.tan((this.camera.fov * Math.PI) / 180 / 2) *
-      this.camera.position.z;
-    this.fovWidth = this.fovHeight * this.camera.aspect;
+			particle.position.y += particle.speed.y;
 
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
+			particle.rotation.x += particle.speed.rotation;
+			particle.rotation.z += particle.speed.rotation;
 
-    this.composer.setSize(window.innerWidth, window.innerHeight);
+			this.checkEdge(particle);
+		}
 
-    if (this.trackball) this.trackball.handleResize();
-  }
+	}
+
+	checkEdge(particle) {
+		if (particle.position.y > 15) {
+			particle.position.y = -15;
+		}
+	}
+
+	createMesh() {
+		let geo = new THREE.TetrahedronBufferGeometry(1, 0);
+		// let mat = new THREE.MeshPhongMaterial();
+		// mat.shininess = 100;
+		let mat = new THREE.MeshPhysicalMaterial({
+			roughness: 0.5,
+			metalness: 0.3,
+			reflectivity: 1,
+			clearcoat: 1
+		});
+		return new THREE.Mesh(geo, mat);
+	}
+
+	initPostProcessing() {
+		this.composer = new EffectComposer(this.renderer);
+		this.composer.enabled = false;
+
+		const renderPass = new RenderPass(this.scene, this.camera);
+		renderPass.renderToScreen = false;
+
+		const contrastEffect = new BrightnessContrastEffect({ contrast: 1 });
+		const contrastPass = new EffectPass(this.camera, contrastEffect);
+		contrastPass.renderToScreen = true;
+
+		this.composer.addPass(renderPass);
+		this.composer.addPass(contrastPass);
+
+		// kickstart composer
+		this.composer.render(1);
+	}
+
+	// ---------------------------------------------------------------------------------------------
+	// EVENT HANDLERS
+	// ---------------------------------------------------------------------------------------------
+
+	resize() {
+		if (!this.renderer) return;
+		this.camera.aspect = window.innerWidth / window.innerHeight;
+		this.camera.updateProjectionMatrix();
+
+		this.fovHeight =
+			2 *
+			Math.tan((this.camera.fov * Math.PI) / 180 / 2) *
+			this.camera.position.z;
+		this.fovWidth = this.fovHeight * this.camera.aspect;
+
+		this.renderer.setSize(window.innerWidth, window.innerHeight);
+
+		// this.composer.setSize(window.innerWidth, window.innerHeight);
+
+		if (this.trackball) this.trackball.handleResize();
+	}
+
+	// ---------------------------------------------------------------------------------------------
+	// PUBLIC
+	// ---------------------------------------------------------------------------------------------
+
+	update() {
+		const delta = this.clock.getDelta();
+		const time = performance.now() * 0.0005;
+
+		if (this.particleCount) {
+			this.updateParticles();
+		}
+
+		if (this.trackball) this.trackball.update();
+	}
+
+	draw() {
+
+		this.renderer.setRenderTarget(this.RenderTriTarget);
+		this.renderer.render(this.particlesRtScene, this.particlesRtCamera);
+		this.renderer.setRenderTarget(null);
+
+		if (this.composer && this.composer.enabled) this.composer.render();
+		else this.renderer.render(this.scene, this.camera);
+
+	}
+
+
 }
