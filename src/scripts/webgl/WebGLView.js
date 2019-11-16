86808aa14885b58839bf2d5289f6610324941568
pfreema1 <paul@level2d.com>
2019-11-16 16:38:24 -0500

    progress

--- src/scripts/webgl/WebGLView.js
+++ src/scripts/webgl/WebGLView.js
@@ -9,243 +9,290 @@ import TweenMax from 'TweenMax';
 import { MeshPhongMaterial } from 'three';
 
 function remap(t, old_min, old_max, new_min, new_max) {
-	let old_range = old_max - old_min;
-	let normalizedT = t - old_min;
-	let normalizedVal = normalizedT / old_range;
-	let new_range = new_max - new_min;
-	let newVal = normalizedVal * new_range + new_min;
-	return newVal;
+  let old_range = old_max - old_min;
+  let normalizedT = t - old_min;
+  let normalizedVal = normalizedT / old_range;
+  let new_range = new_max - new_min;
+  let newVal = normalizedVal * new_range + new_min;
+  return newVal;
 }
 
 export default class WebGLView {
-	constructor(app) {
-		this.app = app;
-		this.PARAMS = {
-			rotSpeed: 0.05
-		};
+  constructor(app) {
+    this.app = app;
+    this.PARAMS = {
+      rotSpeed: 0.05
+    };
+    this.NUM_TEXT_MESHES = 25;
 
-		this.init();
+    this.init();
+  }
 
-	}
+  async init() {
+    this.initThree();
+    this.initBgScene();
+    this.initCubeCamera();
+    this.initObject();
+    this.initLights();
+    this.initTweakPane();
+    await this.loadTextMesh();
+    this.initScenePlane();
+    this.initRenderTri();
+  }
 
-	async init() {
-		this.initThree();
-		this.initBgScene();
-		this.initCubeCamera();
-		this.initObject();
-		this.initLights();
-		this.initTweakPane();
-		await this.loadTextMesh();
-		this.initScenePlane();
-		this.initRenderTri();
-	}
+  initCubeCamera() {
+    this.cubeCamera = new THREE.CubeCamera(0.01, 100, 512);
+    this.cubeCamera.renderTarget.texture.generateMipmaps = true;
+    this.cubeCamera.renderTarget.texture.minFilter =
+      THREE.LinearMipmapLinearFilter;
+    this.bgScene.add(this.cubeCamera);
 
-	initCubeCamera() {
-		this.cubeCamera = new THREE.CubeCamera(0.01, 100, 256);
-		this.cubeCamera.renderTarget.texture.generateMipmaps = true;
-		this.cubeCamera.renderTarget.texture.minFilter = THREE.LinearMipmapLinearFilter;
-		this.bgScene.add(this.cubeCamera);
-	}
+    this.cubeCamera.position.z = 3;
+    this.cubeCamera.rotation.y = Math.PI;
 
-	initScenePlane() {
-		let geo = new THREE.PlaneBufferGeometry(1, 1, 1);
-		this.planeMat = new THREE.MeshBasicMaterial({
-			color: 0xffffff,
-			envMap: this.cubeCamera.renderTarget.texture
-		});
-		this.scenePlane = new THREE.Mesh(geo, this.planeMat);
-		this.bgScene.add(this.scenePlane);
-	}
+    // TweenMax.to(this.cubeCamera.position, 3.0, {
+    //   z: 5,
+    //   repeat: -1
+    // });
+  }
 
-	initTweakPane() {
-		this.pane = new Tweakpane();
+  initScenePlane() {
+    let geo = new THREE.PlaneBufferGeometry(10, 10, 1);
+    this.planeMat = new THREE.MeshBasicMaterial({
+      color: 0xffffff,
+      envMap: this.cubeCamera.renderTarget.texture
+    });
+    this.scenePlane = new THREE.Mesh(geo, this.planeMat);
+    this.bgScene.add(this.scenePlane);
 
-		this.pane
-			.addInput(this.PARAMS, 'rotSpeed', {
-				min: 0.0,
-				max: 0.5
-			})
-			.on('change', value => {
+    document.addEventListener('keydown', e => {
+      if (e.keyCode == 37) {
+        // left
+        this.scenePlane.position.x -= 0.5;
+      } else if (e.keyCode == 38) {
+        // up
+        this.scenePlane.position.y += 0.5;
+      } else if (e.keyCode == 39) {
+        // right
+        this.scenePlane.position.x += 0.5;
+      } else if (e.keyCode == 40) {
+        // down
+        this.scenePlane.position.y -= 0.5;
+      }
 
-			});
-	}
+      console.log(this.scenePlane.position);
+    });
+  }
 
-	initThree() {
-		this.scene = new THREE.Scene();
+  initTweakPane() {
+    this.pane = new Tweakpane();
 
-		this.camera = new THREE.OrthographicCamera();
+    this.pane
+      .addInput(this.PARAMS, 'rotSpeed', {
+        min: 0.0,
+        max: 0.5
+      })
+      .on('change', value => {});
+  }
 
-		this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-		this.renderer.autoClear = true;
+  initThree() {
+    this.scene = new THREE.Scene();
 
-		this.clock = new THREE.Clock();
+    this.camera = new THREE.OrthographicCamera();
 
+    this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+    this.renderer.autoClear = true;
 
-	}
+    this.clock = new THREE.Clock();
+  }
 
-	loadTextMesh() {
-		return new Promise((res, rej) => {
-			let loader = new GLTFLoader();
+  loadTextMesh() {
+    return new Promise((res, rej) => {
+      let loader = new GLTFLoader();
 
-			loader.load('./bbali.glb', object => {
-				object;
-				this.textMesh = object.scene.children[0];
-				console.log(this.textMesh);
-				this.textMesh.add(new THREE.AxesHelper());
-				this.textMesh.rotation.x += Math.PI / 2;
-				this.bgScene.add(this.textMesh);
+      loader.load('./bbali.glb', object => {
+        let textMesh = object.scene.children[0].clone();
+        textMesh.add(new THREE.AxesHelper());
+        textMesh.rotation.x += Math.PI / 2;
 
-				this.textMesh.material = new MeshPhongMaterial();
-				this.textMesh.material.needsUpdate = true;
+        textMesh.material = new MeshPhongMaterial();
+        textMesh.material.needsUpdate = true;
 
-				// this.scene.add(new THREE.Mesh(new BoxGeometry(1, 1, 1), new MeshBasicMaterial()))
+        this.textMeshes = [];
+        let rowSize;
+        let colSize;
+        rowSize = colSize = Math.sqrt(this.NUM_TEXT_MESHES);
 
-				res();
-			});
-		});
-	}
+        for (let i = 0; i < this.NUM_TEXT_MESHES; i++) {
+          let clonedTextMesh = textMesh.clone();
 
-	returnRenderTriGeometry() {
-		const geometry = new THREE.BufferGeometry();
+          clonedTextMesh.position.x += (i % rowSize) * 0.7;
+          clonedTextMesh.position.y += Math.floor(i / colSize) * 0.4;
 
-		// triangle in clip space coords
-		const vertices = new Float32Array([-1.0, -1.0, 3.0, -1.0, -1.0, 3.0]);
+          this.textMeshes.push(clonedTextMesh);
+          this.bgScene.add(clonedTextMesh);
+        }
 
-		geometry.addAttribute('position', new THREE.BufferAttribute(vertices, 2));
+        console.log(this.textMeshes);
 
-		return geometry;
-	}
+        res();
+      });
+    });
+  }
 
-	initRenderTri() {
-		// mostly taken from here: https://medium.com/@luruke/simple-postprocessing-in-three-js-91936ecadfb7
+  returnRenderTriGeometry() {
+    const geometry = new THREE.BufferGeometry();
 
-		this.resize();
-		const geometry = this.returnRenderTriGeometry();
+    // triangle in clip space coords
+    const vertices = new Float32Array([-1.0, -1.0, 3.0, -1.0, -1.0, 3.0]);
 
-		const resolution = new THREE.Vector2();
-		this.renderer.getDrawingBufferSize(resolution);
+    geometry.addAttribute('position', new THREE.BufferAttribute(vertices, 2));
 
-		this.RenderTriTarget = new THREE.WebGLRenderTarget(
-			resolution.x,
-			resolution.y,
-			{
-				format: THREE.RGBFormat,
-				stencilBuffer: false,
-				depthBuffer: true
-			}
-		);
+    return geometry;
+  }
 
-		this.triMaterial = new THREE.RawShaderMaterial({
-			fragmentShader: glslify(fullScreenTriFrag),
-			vertexShader: glslify(fullScreenTriVert),
-			uniforms: {
-				uScene: {
-					type: 't',
-					value: this.bgRenderTarget.texture
-				},
-				uResolution: { value: resolution },
-				uTime: {
-					value: 0.0
-				}
-			}
-		});
+  initRenderTri() {
+    // mostly taken from here: https://medium.com/@luruke/simple-postprocessing-in-three-js-91936ecadfb7
 
-		console.log(this.bgRenderTarget.texture);
+    this.resize();
+    const geometry = this.returnRenderTriGeometry();
 
-		let renderTri = new THREE.Mesh(geometry, this.triMaterial);
-		renderTri.frustumCulled = false;
-		this.scene.add(renderTri);
-	}
+    const resolution = new THREE.Vector2();
+    this.renderer.getDrawingBufferSize(resolution);
 
-	initBgScene() {
-		this.bgRenderTarget = new THREE.WebGLRenderTarget(
-			window.innerWidth,
-			window.innerHeight
-		);
-		this.bgCamera = new THREE.PerspectiveCamera(
-			50,
-			window.innerWidth / window.innerHeight,
-			0.01,
-			100
-		);
-		this.controls = new OrbitControls(this.bgCamera, this.renderer.domElement);
+    this.RenderTriTarget = new THREE.WebGLRenderTarget(
+      resolution.x,
+      resolution.y,
+      {
+        format: THREE.RGBFormat,
+        stencilBuffer: false,
+        depthBuffer: true
+      }
+    );
 
-		this.bgCamera.position.z = 3;
-		this.controls.update();
+    this.triMaterial = new THREE.RawShaderMaterial({
+      fragmentShader: glslify(fullScreenTriFrag),
+      vertexShader: glslify(fullScreenTriVert),
+      uniforms: {
+        uScene: {
+          type: 't',
+          value: this.bgRenderTarget.texture
+        },
+        uResolution: { value: resolution },
+        uTime: {
+          value: 0.0
+        }
+      }
+    });
 
-		this.bgScene = new THREE.Scene();
-	}
+    console.log(this.bgRenderTarget.texture);
 
-	initLights() {
-		this.pointLight = new THREE.PointLight(0xff0000, 1, 100);
-		this.pointLight.position.set(0, 5, 10);
-		this.pointLightHelper = new THREE.PointLightHelper(this.pointLight, 1);
-		this.bgScene.add(this.pointLight);
-		this.bgScene.add(this.pointLightHelper);
-	}
+    let renderTri = new THREE.Mesh(geometry, this.triMaterial);
+    renderTri.frustumCulled = false;
+    this.scene.add(renderTri);
+  }
 
-	initObject() {
-		let geo = new THREE.TetrahedronBufferGeometry(10, 0);
-		let mat = new THREE.MeshPhysicalMaterial({
-			roughness: 0.5,
-			metalness: 0.3,
-			reflectivity: 1,
-			clearcoat: 1
-		});
-		this.tetra = new THREE.Mesh(geo, mat);
-		console.log('tetra:  ', this.tetra);
+  initBgScene() {
+    this.bgRenderTarget = new THREE.WebGLRenderTarget(
+      window.innerWidth,
+      window.innerHeight
+    );
+    this.bgCamera = new THREE.PerspectiveCamera(
+      50,
+      window.innerWidth / window.innerHeight,
+      0.01,
+      100
+    );
+    this.controls = new OrbitControls(this.bgCamera, this.renderer.domElement);
 
-		// this.bgScene.add(this.tetra);
-	}
+    this.bgCamera.position.z = 3;
+    this.controls.update();
 
-	resize() {
-		if (!this.renderer) return;
-		this.camera.aspect = window.innerWidth / window.innerHeight;
-		this.camera.updateProjectionMatrix();
+    this.bgScene = new THREE.Scene();
+  }
 
-		this.fovHeight =
-			2 *
-			Math.tan((this.camera.fov * Math.PI) / 180 / 2) *
-			this.camera.position.z;
-		this.fovWidth = this.fovHeight * this.camera.aspect;
+  initLights() {
+    this.pointLight = new THREE.PointLight(0xff0000, 1, 100);
+    this.pointLight.position.set(0, 5, 10);
+    this.pointLightHelper = new THREE.PointLightHelper(this.pointLight, 1);
+    this.bgScene.add(this.pointLight);
+    this.bgScene.add(this.pointLightHelper);
+  }
 
-		this.renderer.setSize(window.innerWidth, window.innerHeight);
+  initObject() {
+    let geo = new THREE.TetrahedronBufferGeometry(10, 0);
+    let mat = new THREE.MeshPhysicalMaterial({
+      roughness: 0.5,
+      metalness: 0.3,
+      reflectivity: 1,
+      clearcoat: 1
+    });
+    this.tetra = new THREE.Mesh(geo, mat);
+    console.log('tetra:  ', this.tetra);
 
-		if (this.trackball) this.trackball.handleResize();
-	}
+    // this.bgScene.add(this.tetra);
+  }
 
-	updateTetra() {
-		this.tetra.rotation.y += this.PARAMS.rotSpeed;
-	}
+  resize() {
+    if (!this.renderer) return;
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
 
-	update() {
-		const delta = this.clock.getDelta();
-		const time = performance.now() * 0.0005;
+    this.fovHeight =
+      2 *
+      Math.tan((this.camera.fov * Math.PI) / 180 / 2) *
+      this.camera.position.z;
+    this.fovWidth = this.fovHeight * this.camera.aspect;
 
-		this.controls.update();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
 
-		if (this.triMaterial) {
-			this.triMaterial.uniforms.uTime.value = time;
-		}
+    if (this.trackball) this.trackball.handleResize();
+  }
 
-		if (this.tetra) {
-			this.updateTetra();
-		}
+  updateTetra() {
+    this.tetra.rotation.y += this.PARAMS.rotSpeed;
+  }
 
-		if (this.planeMat) {
-			this.cubeCamera.update(this.renderer, this.bgScene);
+  animateTextMeshes(time) {
+    let dur = 4;
 
-		}
+    for (let i = 0; i < this.textMeshes.length; i++) {
+      let textMesh = this.textMeshes[i];
 
-		if (this.trackball) this.trackball.update();
-	}
+      textMesh.position.z = Math.abs(Math.sin(time + i));
+    }
+  }
 
-	draw() {
-		this.renderer.setRenderTarget(this.bgRenderTarget);
-		this.renderer.render(this.bgScene, this.bgCamera);
-		this.renderer.setRenderTarget(null);
+  update() {
+    const delta = this.clock.getDelta();
+    const time = performance.now() * 0.0005;
 
-		this.renderer.render(this.scene, this.camera);
+    this.controls.update();
 
-	}
+    if (this.triMaterial) {
+      this.triMaterial.uniforms.uTime.value = time;
+    }
+
+    if (this.tetra) {
+      this.updateTetra();
+    }
+
+    if (this.planeMat) {
+      this.cubeCamera.update(this.renderer, this.bgScene);
+    }
+
+    if (this.textMeshes) {
+      this.animateTextMeshes(time);
+    }
+
+    if (this.trackball) this.trackball.update();
+  }
+
+  draw() {
+    this.renderer.setRenderTarget(this.bgRenderTarget);
+    this.renderer.render(this.bgScene, this.bgCamera);
+    this.renderer.setRenderTarget(null);
+
+    this.renderer.render(this.scene, this.camera);
+  }
 }
